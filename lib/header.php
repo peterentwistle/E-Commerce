118cfad0bc20ea32958a0ff0710598283a5a2ef3
Rover656 <reece-mackie@hotmail.com>
2017-03-10 18:20:43 +0000

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -2,3 +2,11 @@
 //Just a placeholder for making the site more modular
 ?>
 <h1>SITE NAME</h1>
+<div id="header">
+	<p>Header</p>
+	<ul>
+		<li><a href="<?php echo 'index.php'; ?>">Home</li>
+		<li><a href="<?php echo 'shop.php'; ?>">Shop</a></li>
+		<li><a href="<?php echo 'basket.php'; ?>">Basket</a></li>
+	</ul>
+</div>
