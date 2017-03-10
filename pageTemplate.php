984caa10fcb66f8036ff4e610d66687e48d6ef9c
Rover656 <reece-mackie@hotmail.com>
2017-03-10 18:15:35 +0000

    Add the Page Template as a base

---
+++ pageTemplate.php
@@ -0,0 +1,33 @@
+<?php
+// Begin the site session
+session_start();
+//TODO: Update the inc folder then migrate lib to inc
+include_once("inc/mainIncludes.php");
+include_once("inc/shopIncludes.php");
+?>
+<DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" <?php echo $main_css; ?>>
+<title><?php echo $shopTitle; ?></title>
+</head>
+
+<body>
+<?php
+	//Load in the header
+	require_once('lib/header.php');
+?>
+
+<h1>Featured Items</h1>
+<?php
+//TODO: Update to use database items
+echo $shop->randItems($items, $currency, $currency_format);
+?>
+
+<?php
+	require_once('lib/footer.php');
+?>
+
+</body>
+</html>
