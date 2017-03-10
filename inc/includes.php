fad413a8329afc62baeb2ff47f5f1b380f05605e
Rover656 <reece-mackie@hotmail.com>
2017-03-10 19:28:32 +0000

    Fix Includes File

--- inc/includes.php
+++ inc/includes.php
@@ -1,21 +1,22 @@
 <?php
+	/////TODO: FIX DEFINED LOCATIONS (NEED FOR SUBDIRECTORIES I THINK)
 	//This script just adds all the config into a massive array for easy access
 	//Normal Includes
-	require_once(BASE_URL . 'core/config.php');
+	require_once('/core/config.php');
 	$siteData[0] = $shopTitle;
 	$siteData[1] = $footerCopy;
 	$siteData[2] = $currency;
 	$siteData[3] = $currency_format;
 	$main_css = 'href="style/css/main.css"';
-	include_once("core/config.php");
+	include_once("/core/config.php");
     //////Commented out for now include_once("core/dbcon.php");
 	//Basket Includes
-	require_once("core/items.php");
-	require_once("core/classes/Basket.php");
-	require_once("core/classes/Shop.php");
+	require_once("/core/items.php");
+	require_once("/core/classes/Basket.php");
+	require_once("/core/classes/Shop.php");
 	$basket = new Basket;
 	//Shop Includes
-	include_once("core/items.php");
-	include_once("core/classes/Shop.php");
+	include_once("/core/items.php");
+	include_once("/core/classes/Shop.php");
 	$shop = new Shop;
 ?>
