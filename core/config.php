288a4ac052924fb95fc03aebb99ad846c8d8a2fe
Rover656 <reece-mackie@hotmail.com>
2017-03-10 19:19:32 +0000

    Make config nicer

--- core/config.php
+++ core/config.php
@@ -1,29 +1,19 @@
 <?php
-// Configuration file - Will be changed to load in from the DB
-define("BASE_URL", "/");
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
+//////Configuration file - Will be changed to load in from the DB
+////Changable by User - For help see GitHub WiKi
+//Shop Details
 $shopTitle = "E-Commerce the Open Source Project";
 $footerCopy = '&copy;'.date('Y').' Peter Entwistle and Rover656. Check out the source code on <a href="http://github.com/Rover656/E-Commerce">GitHub</a>'; //GitHub link to the Rover656 Fork
-/*
-Set shop currency
-Currencies available:
-GBP - £
-USD - $
-EUR - €
-More currency support coming soon
-*/
+//Set shop currency
 $currency = "GBP";
-/* 
-Set the currency format
-"english" (default)
-	e.g. 1,235.56
-
-"european" notation
-	e.g. 1.234,56
-
-"eng_without_thous" english notation without thousands separator
-	e.g. 1234.56
-*/
+//Set the currency format
 $currency_format = "english";
-
+//Database Details
+$db_host = "localhost";
+$db_user = "DBUSER";
+$db_pass = "CHANGEME";
+$db_name = "DB_NAME";
+////DO NOT CHANGE
+define("BASE_URL", "/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
 ?>
