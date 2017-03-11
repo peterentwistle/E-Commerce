9a8be61b5c1aa9a000293ee90acfde85e4e4d83c
Rover656 <reece-mackie@hotmail.com>
2017-03-11 16:42:02 +0000

    Login Checker

--- core/classes/User.php
+++ core/classes/User.php
@@ -3,10 +3,26 @@ class User {
 	public function DoLogin($username, $password) {
 		require_once("/inc/includes.php");
 		global $dbcon;
+		$userToken = GenerateToken();
 	}
 	public function DoLogout($token) {
 		require_once("/inc/includes.php");
 		global $dbcon;
+	}
+	public function CheckToken($token) {
+		require_once("inc/includes.php");
+		global $dbcon;
+		$sql = "SELECT Token FROM logintokens WHERE Token='{$token}'";
+		$result = $dbcon->query($sql);
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				//TODO: Make it so that there is a token expiry time
+				return true;
+			}
+		} else {
+			return false;
+		}
 	}
 	public function GenerateToken() {
 		require_once("/inc/includes.php");
