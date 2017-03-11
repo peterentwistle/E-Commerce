ff0ddb29d04bbc7fe24f119e54593711e8d5a8f9
Rover656 <reece-mackie@hotmail.com>
2017-03-11 16:39:58 +0000

    Token Checker

--- core/classes/User.php
+++ core/classes/User.php
@@ -11,6 +11,17 @@ class User {
 	public function GenerateToken() {
 		require_once("/inc/includes.php");
 		global $dbcon;
+		$token = md5(microtime());
+		$sql = "SELECT Token FROM logintokens WHERE Token='{$token}'";
+		$result = $dbcon->query($sql);
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				return GenerateToken();
+			}
+		} else {
+			return $token;
+		}
 	}
 }
 ?>
