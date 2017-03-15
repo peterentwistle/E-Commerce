f97b76833a6e3fa68b064adcd98a3bff7ad433e4
Rover656 <reece-mackie@hotmail.com>
2017-03-15 09:37:19 +0000

    Update includes.php

--- inc/includes.php
+++ inc/includes.php
@@ -2,29 +2,27 @@
 	/////TODO: FIX DEFINED LOCATIONS (NEED FOR SUBDIRECTORIES I THINK)
 	//This script just adds all the config into a massive array for easy access
 	//Normal Includes
-	require_once('/core/config.php');
+	require_once($_SERVER["DOCUMENT_ROOT"]' . '/core/config.php');
 	$siteData[0] = $shopTitle;
 	$siteData[1] = $footerCopy;
 	$siteData[2] = $currency;
 	$siteData[3] = $currency_format;
-	$main_css = 'href="' . $themeUri . 'style/css/main.css"';
-	require_once("/core/config.php");
-    require_once("/core/dbcon.php");
+    	require_once("$_SERVER["DOCUMENT_ROOT"] . '/core/dbcon.php');
 	//Basket Includes
-	require_once("/core/items.php");
-	require_once("/core/classes/Basket.php");
-	require_once("/core/classes/Shop.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/items.php');
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/classes/Basket.php');
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/classes/Shop.php');
 	$basket = new Basket;
 	//Shop Includes
-	require_once("/core/items.php");
-	require_once("/core/classes/Shop.php");
-	require_once("/core/classes/Item.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/items.php');
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/classes/Shop.php');
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/classes/Item.php');
 	$Item = new Item;
 	$shop = new Shop;
 	//Encryption Includes
-	require_once("/core/classes/REnc.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]. '/core/classes/REnc.php');
 	$REnc = new REnc;
 	//Currency Includes
-	require_once("core/classes/Currency.php");
+	require_once($_SERVER["DOCUMENT_ROOT"]. 'core/classes/Currency.php');
 	$Currency = new Currency;
 ?>
