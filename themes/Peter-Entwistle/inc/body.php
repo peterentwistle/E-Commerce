08a0965e948de2cbed71f9fdffde738beeeb6aca
Rover656 <reece-mackie@hotmail.com>
2017-03-15 09:30:05 +0000

    Update body.php

--- themes/Peter-Entwistle/inc/body.php
+++ themes/Peter-Entwistle/inc/body.php
@@ -1,13 +1,7 @@
 <?php
-	include_once($themeUri . "inc/header.php");
-?>
-<?php
 if (isset($_GET['src'])) {
 	include_once($_GET['src']);
 } else {
 	include_once("/pages/index.php");
 }
-?>
-<?php
-	include_once($themeUri . "inc/footer.php");
 ?>
